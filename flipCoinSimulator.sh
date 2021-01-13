fd0016c0e5fd574a5a0f8145db72398bc1ee6c89
“robin-31” <rm.robinmanna01@gmail.com>
2021-01-13 10:56:10 +0530

    by coin flip deciding winner or losser

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,2 +1,10 @@
 #!/bin/bash -x
 echo "welcome to flip coin problem"
+isHead=1
+flipcoin=$((RANDOM%2))
+if [ "$isHead" -eq "$flipcoin" ]
+then
+	echo "it's head you are winner"
+else
+	echo "it's tail you are losser"
+fi
