1e5c1514cab1c364988249be43a3d12f7963aec3
“robin-31” <rm.robinmanna01@gmail.com>
2021-01-13 12:35:27 +0530

    ADDED: tie condition is added

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -3,6 +3,9 @@ echo "welcome to flip coin problem"
 isHead=1
 headcounter=0
 tailcounter=0
+deciderhead=0
+decidertail=0
+deciderresult=2
 maxwin=21
 while [[ $headcounter -ne $maxwin && $tailcounter -ne $maxwin ]]
 do
@@ -14,9 +17,27 @@ do
 		((tailcounter++))
 	fi
 done
+#for checking tie condition is working properly
+#headcounter=5
+#tailcounter=5
 if [ $headcounter -eq $tailcounter ]
 then
 	echo "it's tie"
+	while [[ $deciderhead -ne $deciderresult && $decidertail -ne $deciderresult ]]
+	do
+
+		flipcoin=$((RANDOM%2))
+		if [ "$isHead" -eq "$flipcoin" ]
+		then
+			echo "it's head"
+			((deciderhead++))
+		else
+			echo "it's tail"
+			((decidertail++))
+		fi
+	done
+	echo $deciderhead
+	echo $decidertail
 elif [ $tailcounter -eq $maxwin ]
 then
 	echo "tail is winner $tailcounter"
