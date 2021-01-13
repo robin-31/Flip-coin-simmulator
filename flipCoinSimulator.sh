e250c3a6d1cd0c131261770e806990ebe4588e97
“robin-31” <rm.robinmanna01@gmail.com>
2021-01-13 11:07:02 +0530

    tells how many times heads and tails occur

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,19 @@
 #!/bin/bash -x
 echo "welcome to flip coin problem"
 isHead=1
-flipcoin=$((RANDOM%2))
-if [ "$isHead" -eq "$flipcoin" ]
-then
-	echo "it's head you are winner"
-else
-	echo "it's tail you are losser"
-fi
+headcounter=0
+tailcounter=0
+for (( i=1; i<=10; i++ ))
+do
+	flipcoin=$((RANDOM%2))
+	if [ "$isHead" -eq "$flipcoin" ]
+	then
+		echo "it's head you are winner"
+		((headcounter++))
+	else
+		echo "it's tail you are losser"
+		((tailcounter++))
+	fi
+done
+echo $headcounter
+echo $tailcounter
